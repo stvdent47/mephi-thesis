1c53ee2b4e7833f7570a1772f3f242de97a08c39
Anton Rodionov <stvdent47dev@gmail.com>
2026-04-12 00:45:05 +0400

    add account type schema

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,4 +1,9 @@
+import { Type } from "@sinclair/typebox";
+import { AccountType } from "../generated/prisma/client.js";
+
 export interface UserIdentity {
 	id: string;
 	email: string;
 }
+
+export const AccountTypeSchema = Type.Enum(AccountType);
