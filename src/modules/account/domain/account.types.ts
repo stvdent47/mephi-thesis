4b139e298d02b05e51fa4dc5f561584d99300490
Anton Rodionov <stvdent47dev@gmail.com>
2026-04-12 00:29:35 +0400

    add account types and interfaces for account management

---
+++ src/modules/account/domain/account.types.ts
@@ -0,0 +1,47 @@
+import type { AccountType } from "../../../generated/prisma/enums.js";
+
+export interface AccountRecord {
+	id: string;
+	userId: string;
+	name: string;
+	type: AccountType;
+	currency: string;
+	balance: number;
+	isActive: boolean;
+	createdAt: number;
+	updatedAt: number;
+}
+
+export interface CreateAccountCommand {
+	name: string;
+	type: AccountType;
+	currency?: string;
+}
+
+export interface CreateAccountResult extends AccountRecord { }
+
+export interface FindAllAccountsResult extends AccountRecord { }
+
+export interface FindAccountByIdResult extends AccountRecord { }
+
+export interface UpdateAccountCommand {
+	name?: string;
+	type?: AccountType;
+	isActive?: boolean;
+}
+
+export interface UpdateAccountResult extends AccountRecord { }
+
+export interface DeleteAccountResult extends AccountRecord { }
+
+export interface CreateAccountInput {
+	name: string;
+	type: AccountType;
+	currency?: string;
+}
+
+export interface UpdateAccountInput {
+	name?: string;
+	type?: AccountType;
+	isActive?: boolean;
+}
