9a4d1b56e22902b0985d00857ee33b83390b13b3
Anton Rodionov <stvdent47dev@gmail.com>
2026-05-01 02:29:38 +0400

    add forecasting lstm types

--- src/modules/forecasting/domain/forecasting.types.ts
+++ src/modules/forecasting/domain/forecasting.types.ts
@@ -1,5 +1,6 @@
 export const enum ForecastingMethod {
 	SMA = 'sma',
+	LSTM = 'lstm',
 }
 
 export interface PredictionItem {
@@ -35,7 +36,22 @@ export interface SMAForecastQuery extends ForecastQuery { }
 
 export interface SMAForecastResult {
 	method: ForecastingMethod.SMA;
-	parameters: { windowSize: number; };
+	parameters: { windowSize: number };
+	totalForecast: PredictionItem[];
+	categoryForecasts: CategoryForecastItem[];
+	historicalData: HistoricalItem[];
+	accuracy?: AccuracyMetrics;
+}
+
+// LSTM
+export interface LSTMForecastQuery extends ForecastQuery {
+	epochs: number;
+	lookback: number;
+}
+
+export interface LSTMForecastResult {
+	method: ForecastingMethod.LSTM;
+	parameters: { lookback: number; epochs: number; trainingLoss: number };
 	totalForecast: PredictionItem[];
 	categoryForecasts: CategoryForecastItem[];
 	historicalData: HistoricalItem[];
